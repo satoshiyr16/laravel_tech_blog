a78ae00526c5c8bc37cc82194b41b200a89265eb
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-04-02 18:19:08 +0900

    🎨 【フロント】ホーム画面に動的背景、波線のエフェクト追加 & それに伴いbaseのlayout作成

--- src/resources/js/components/front/HomePage.jsx
+++ src/resources/js/components/front/HomePage.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
-import ParticlesBackground from './effects/ParticleBackground';
-import '@css/reset.css';
+import React, { useEffect, useState } from 'react';
+import BaseLayout from '@components/front/layouts/BaseLayout';
+import WaveEffect from '@components/front/effects/WaveEffect';
+import '@css/front/HomePage.css';
 
 function HomePage() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  const [selectedNav, setSelectedNav] = useState('Home');
+
+  const navItems = [
+    { name: 'Home', url: '/' },
+    { name: 'Content', url: '/#' },
+    { name: 'Search', url: '/#' },
+    { name: 'Profile', url: '/#' },
+    { name: 'Contact', url: '/#' },
+  ];
+
   return (
-    <div className="home-page-container">
-      <ParticlesBackground />
-    </div>
+    <BaseLayout>
+      <div className="l-home_page_area">
+        <WaveEffect />
+        <div className="l-title_area">
+          <h2 className={`title ${isVisible ? '-visible' : ''}`}>
+            <span>T</span>
+            <span>E</span>
+            <span>C</span>
+            <span>H</span>
+            <span>&nbsp;</span>
+            <span>B</span>
+            <span>L</span>
+            <span>O</span>
+            <span>G</span>
+          </h2>
+        </div>
+        <div className="l-nav_area">
+          <nav>
+            <ul>
+              {navItems.map((item) => (
+                <li
+                  key={item.name}
+                  className={selectedNav === item.name ? 'current' : ''}
+                  onClick={() => setSelectedNav(item.name)}
+                >
+                  <a href={item.url}>{item.name}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </BaseLayout>
   );
 }
 
---
+++ src/resources/js/components/front/effects/WaveEffect.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+
+const WaveComponent = () => {
+  useEffect(() => {
+    var unit = 100,
+      canvas,
+      info = {},
+      color;
+
+    function init() {
+      info.seconds = 0;
+      info.t = 0;
+      canvas = document.getElementById("waveCanvas");
+      color = ['#4B4E6D', '#84DCC6', '#95A3B3', '#B9E3C6', '#F1B5CB'];
+
+      canvas.width = document.documentElement.clientWidth;
+      canvas.height = 200;
+      canvas.contextCache = canvas.getContext("2d");
+
+      update();
+    }
+
+    function update() {
+      draw(canvas, color);
+
+      info.seconds = info.seconds + .028;
+      info.t = info.seconds * Math.PI;
+      setTimeout(update, 35);
+    }
+
+    function draw(canvas, color) {
+      var context = canvas.contextCache;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      drawWave(canvas, color[0], 0.5, 3, 0);
+      drawWave(canvas, color[1], 0.8, 4, 0);
+      drawWave(canvas, color[2], 0.5, 1.6, 0);
+      drawWave(canvas, color[3], 0.8, 3, 100);
+      drawWave(canvas, color[4], 0.5, 1.6, 250);
+    }
+
+    function drawWave(canvas, color, alpha, zoom, delay) {
+      var context = canvas.contextCache;
+      context.strokeStyle = color;
+      context.lineWidth = 1;
+      context.globalAlpha = alpha;
+      context.beginPath();
+      drawSine(canvas, info.t / 0.5, zoom, delay);
+      context.stroke();
+    }
+
+    function drawSine(canvas, t, zoom, delay) {
+      var xAxis = Math.floor(canvas.height / 2);
+      var context = canvas.contextCache;
+      var x = t;
+      var y = Math.sin(x) / zoom;
+      context.moveTo(0, unit * y + xAxis);
+
+      for (var i = 0; i <= canvas.width + 10; i += 10) {
+        x = t + (-xAxis + i) / unit / zoom;
+        y = Math.sin(x - delay) / 3;
+        context.lineTo(i, unit * y + xAxis);
+      }
+    }
+
+    init();
+  }, []);
+
+  return <canvas id="waveCanvas"></canvas>;
+};
+
+export default WaveComponent;
