2b40633e110adfeb8d5572ea48f0faf969273d7e
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-04-07 22:39:00 +0900

    🎨 フロント側それぞれのページ用意

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -1,28 +1,39 @@
 import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from '@components/front/HomePage';
+import HomePage from '@components/front/templates/HomePage';
+import ListPost from '@components/front/templates/ListPost';
+import SearchPost from '@components/front/templates/SearchPost';
+import Profile from '@components/front/templates/Profile';
+import Contact from '@components/front/templates/Contact';
 import LoginForm from '@components/admin/auth/LoginForm';
-import AdminHomePage from '@components/admin/HomePage';
-import ProtectedRoute from '@components/admin/auth/ProtectedRoute';
+import AdminHome from '@components/admin/templates/AdminHome';
+// import ProtectedRoute from '@components/admin/auth/ProtectedRoute';
 import { AuthProvider } from '@components/admin/auth/AuthContext';
 
 
 function App() {
   return (
-  <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin/login" element={<LoginForm />} />
-          <Route path="/admin/home" element={
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* ======フロント側====== */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/content" element={<ListPost />} />
+          <Route path="/search" element={<SearchPost />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* ======管理側====== */}
+          <Route path="/admin/login" element={<LoginForm />} />
+          <Route path="/admin" element={<AdminHome />} />
+          {/* <Route path="/admin" element={
             <ProtectedRoute>
               <AdminHomePage />
             </ProtectedRoute>
-          } />
-      </Routes>
-    </BrowserRouter>
-  </AuthProvider>
+          } /> */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
